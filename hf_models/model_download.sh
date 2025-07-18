8c303055e17d896f7d265c8b03f4aa3e0c04bb47
Zichen Wen <Zichen.Wen@outlook.com>
2025-07-18 17:25:51 +0800

    init commit

---
+++ hf_models/model_download.sh
@@ -0,0 +1,4 @@
+huggingface-cli download GSAI-ML/LLaDA-8B-Instruct --local-dir ./LLaDA-8B-Instruct --resume-download
+huggingface-cli download GSAI-ML/LLaDA-1.5 --local-dir ./LLaDA-1.5 --resume-download
+huggingface-cli download Dream-org/Dream-v0-Instruct-7B --local-dir ./Dream-v0-Instruct-7B --resume-download
+huggingface-cli download Gen-Verse/MMaDA-8B-MixCoT --local-dir ./MMaDA-8B-MixCoT --resume-download
